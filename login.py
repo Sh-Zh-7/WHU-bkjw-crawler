989634300d74d2f10d668794efcfb4eb09255137
sh-zh-7 <2431297348@qq.com>
2020-04-16 22:04:45 +0800

    Add login failed reason

--- login.py
+++ login.py
@@ -6,13 +6,16 @@ import utils
 
 # 各种资源的url
 url_home = "http://bkjw.whu.edu.cn/"
-url_image = url_home + "servlet/_33cb6c3?v=2"
-url_login = url_home + "servlet/_9b11715648"
+url_image = url_home + "servlet/_89ab36cec99d8d?v=2"
+url_login = url_home + "servlet/_90e916fe3"
 url_index = url_home + "stu/stu_index.jsp"
+url_success = url_home + "servlet/../stu/stu_index.jsp"
+
 
 def GetCAPTCHA(session, url=url_image):
     # 设置一个User-Agent, 伪装成浏览器
     # 这是第一次访问, 所以要记录cookies
+    print(url)
     response = session.get(url=url, headers=utils.header, stream=True)
     captcha = response.content
     cookie = response.cookies
@@ -58,20 +61,24 @@ def Login(user, pwd):
     session = requests.session()
     # 获取验证码
     image, cookie = GetCAPTCHA(session, url_image)
-    # TODO: 给用户更多的选择
     captcha_content = input("请输入验证码: ")
-
     # 将密码加密后登录
     encrypted_pwd = utils.EncryptPassword(pwd)
     login = SendPost(user, encrypted_pwd, captcha_content, cookie, session=session)
-    print(login.url)
-
-    # 获取csrf_token和的登录后的cookie并返回
-    login_cookie = login.cookies
-    return session, login_cookie, GetToken(login_cookie, session=session)
+    if login.url == url_success:
+        # 获取csrf_token和的登录后的cookie并返回
+        login_cookie = login.cookies
+        return session, login_cookie, GetToken(login_cookie, session=session)
+    else:
+        print("登录失败!")
+        failed_content = login.text
+        soup = bs(failed_content, "html.parser")
+        reason = soup.select("#loginInputBox > tr:nth-child(4) > td > font")[0].get_text()
+        print(reason)
+        exit(0)
 
 
 if __name__ == "__main__":
-    user = "2018302080181"
+    user = "2018302080182"
     password = "20000721"
     Login(user, password)
